aea6c5193b4929261c2a455e0f4d8ff0da7aaf55
Sedrak Adoyan <set.adoyan@gmail.com>
2023-07-04 18:35:04 +0400

    fix: return burger menu

--- Js/jquery.js
+++ Js/jquery.js
@@ -1,4 +1,29 @@
 $(function () {
+  $(".menu__btn").on("click", function () {
+    $(".menu__list").toggleClass("menu__list--active");
+    $(this).toggleClass("active");
+  });
+
+  $(".open-popup").click(function (e) {
+    e.preventDefault();
+    $(".popup-bg").fadeIn(800);
+  });
+
+  $(".close-popup").click(function () {
+    $(".popup-bg").fadeOut(800);
+  });
+
+  $(".open-popup").click(function (e) {
+    e.preventDefault();
+    $(".popup-bg").fadeIn(800);
+    $("html").addClass("no-scroll");
+  });
+
+  $(".close-popup").click(function () {
+    $(".popup-bg").fadeOut(800);
+    $("html").removeClass("no-scroll");
+  });
+
   //BEGIN
   $(".ask").on("click", function (e) {
     e.preventDefault();
